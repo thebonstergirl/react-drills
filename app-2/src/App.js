d9136a3538cf43087d9c0b15e6cee1c441911f50
Bonnie Jordan <thebonstergirl@gmail.com>
2018-08-14 14:25:07 -0500

    done with this one, too

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -3,16 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      arr1: ["Hello", "Darkness", "My", "Old", "Friend"],
+    }
+  }
+
+processList(){
+  return this.state.arr1.map((e, i, arr) => <h2> {arr[i]} </h2>); 
+}
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <p>{this.processList()}</p>
       </div>
     );
   }
